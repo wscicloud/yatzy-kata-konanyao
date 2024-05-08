9d3a74b611fb8d96421dc119018690c9c774add0
wsci.cloud <95191398+wscicloud@users.noreply.github.com>
2024-05-08 15:27:36 +0200

    Refactorer le code

---
+++ src/main/java/com/yatzy/kata/CategoryEnum.java
@@ -0,0 +1,32 @@
+package com.yatzy.kata;
+
+import java.util.function.Function;
+
+public enum CategoryEnum {
+	CHANGE(YatzyScore::chance),
+	YATZY(YatzyScore::yatzy),
+	ONES(YatzyScore::ones),
+	TWOS(YatzyScore::twos),
+	THREES(YatzyScore::threes),
+	FOURS(YatzyScore::fours),
+	FIVES(YatzyScore::fives),
+	SIXES(YatzyScore::sixes),
+	PAIR(YatzyScore::pair),
+	TOW_PAIRS(YatzyScore::twoPairs),
+	THREE_OF_A_KIND(YatzyScore::threeOfAKind),
+	FOUR_OF_A_KIND(YatzyScore::fourOfAKind),
+	SMALL_STRAIGHT(YatzyScore::smallStraight),
+	LARGE_STRAIGHT(YatzyScore::largeStraight),
+	FULL_HOUSE(YatzyScore::fullHouse);
+	
+	CategoryEnum(Function<Roll, Integer> computScore){
+		this.computeScore = computScore;
+	}
+	
+	public int scoreCalculator(Roll roll) {
+		return this.computeScore.apply(roll);
+	}
+
+	private final Function<Roll, Integer> computeScore;
+	
+}
